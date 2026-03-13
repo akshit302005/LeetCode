68d820a38e6c16ba18050b2349fc7ec161ffff85
akshit302005 <156989371+akshit302005@users.noreply.github.com>
2026-03-13 11:36:02 +0530

    Time: 15 ms (5.66%) | Memory: 47.7 MB (7.43%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> list = new ArrayList<>();
+        for(int num:asteroids){
+            list.add(num);
+        }
+        int i=1;
+        while(i<list.size()){
+            if(list.get(i)<0 && list.get(i-1)>0){
+                int num1=Math.abs(list.get(i));
+                int num2=Math.abs(list.get(i-1));
+               if(num2>num1){
+                list.remove(i);
+               }else if(num1>num2){
+                list.remove(i-1);
+                if(i>1) i--;
+               }else{
+                list.remove(i);
+                list.remove(i-1);
+                if(i>1) i--;
+               }
+            }else{
+                i++;
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int j = 0; j < list.size(); j++) {
+            result[j] = list.get(j);
+        }
+        return result;
+    }
+}
