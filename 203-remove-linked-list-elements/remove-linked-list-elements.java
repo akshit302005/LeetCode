ec173a1c01025e0b60ad10f116502485c455a2da
akshit302005 <156989371+akshit302005@users.noreply.github.com>
2026-03-23 10:16:04 +0530

    Time: 1 ms (96.23%) | Memory: 47.1 MB (94.25%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -8,24 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+//         List<Integer> list = new ArrayList<>();
+//         ListNode temp = head;
+//         while(temp!=null){
+//             if(temp.val==val){
+//                 temp=temp.next;
+//             }else{
+//                 list.add(temp.val);
+//                 temp=temp.next;
+//             }
+//         }
+//         ListNode dummy = new ListNode(0);
+//         ListNode dummy1=dummy;
+//         for(int i=0;i<list.size();i++){
+//             dummy1.next=new ListNode(list.get(i));
+//             dummy1=dummy1.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        List<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            if(temp.val==val){
-                temp=temp.next;
-            }else{
-                list.add(temp.val);
-                temp=temp.next;
-            }
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode dummy1=dummy;
-        for(int i=0;i<list.size();i++){
-            dummy1.next=new ListNode(list.get(i));
-            dummy1=dummy1.next;
-        }
-        return dummy.next;
+        if(head==null) return head;
+        head.next=removeElements(head.next,val);
+        if(head.val==val) return head.next;
+        return head;
     }
 }
