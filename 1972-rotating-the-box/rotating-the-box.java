a9592a71b21c407650d350a4e0dfe8b34afd1983
akshit302005 <156989371+akshit302005@users.noreply.github.com>
2026-05-06 10:46:46 +0530

    Time: 468 ms (5.05%) | Memory: 125 MB (28.12%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,34 +1,22 @@
 class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
-        int n = boxGrid[0].length;
-        for(char[] arr : boxGrid) {
-            int i = 0;
-            for(int j = 0; j < n; j++) {
-                if(arr[j] == '*') {
-                    i = j+1;
-                } else if (arr[j] != '#') {
-                    char temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    i++;
+        // int var = 0;
+       for(int i = 0 ; i < boxGrid.length ; i++) {
+            for(int t = 0 ; t < boxGrid[i].length ; t++) {
+                for(int j = boxGrid[i].length - 1 ; j > 0 ; j--) {
+                    if(boxGrid[i][j] == '.' && boxGrid[i][j - 1] == '#') {
+                        char temp = boxGrid[i][j];
+                        boxGrid[i][j] = boxGrid[i][j - 1];
+                        boxGrid[i][j - 1] = temp;
+                    }
                 }
             }
         }
-        n = boxGrid.length;
-        int m = boxGrid[0].length;
+        int n = boxGrid.length, m = boxGrid[0].length;
         char[][] box = new char[m][n];
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                box[i][j] = boxGrid[j][i];
-            }
-        }
-        for(char[] arr : box) {
-            int i = 0, j = arr.length-1;
-            while(i<j) {
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++; j--;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                box[j][n-1-i] = boxGrid[i][j];
             }
         }
         return box;
