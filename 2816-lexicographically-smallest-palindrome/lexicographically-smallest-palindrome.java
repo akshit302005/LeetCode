d7f1b73f741d1a9ec8be1d2d71572c9f572528df
akshit302005 <156989371+akshit302005@users.noreply.github.com>
2026-05-07 10:28:59 +0530

    Time: 9 ms (17.89%) | Memory: 47.1 MB (23.20%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        char[] arr = new char[s.length()];
+        while(left <= right){
+            if(s.charAt(left) == s.charAt(right)) {
+                 arr[right] = s.charAt(left);
+                 arr[left] = s.charAt(left);
+            }
+            else{
+                if(s.charAt(left) < s.charAt(right)) {
+                    arr[left] = s.charAt(left);
+                    arr[right] = s.charAt(left);
+                }else{
+                    arr[left] = s.charAt(right);
+                    arr[right] = s.charAt(right);
+                }
+            }
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}
